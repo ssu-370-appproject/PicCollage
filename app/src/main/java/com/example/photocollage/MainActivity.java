45aa7638ecc8eee83a46f945f392e7ccf586977e
rafad900 <rafad900@gmail.com>
2019-04-09 08:37:58 -0700

    The first commit, might have a problem with the emulator, or maybe its just my crappy laptop Write your name when pushing the commit btw

---
+++ app/src/main/java/com/example/photocollage/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.photocollage;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                openCollageChooser();
+            }
+        }, 2000);
+    }
+    
+
+    public void openCollageChooser() {
+        Intent intent = new Intent(this, CollageChooser.class);
+        startActivity(intent);
+    }
+}
