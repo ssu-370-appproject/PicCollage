b8fecebe5a6100b442d116bbb57d1f55c9e690d0
rafad900 <rafad900@gmail.com>
2019-04-08 20:03:14 -0700

    The first commit, might have a problem with the emulator, or maybe its just my crappy laptop Write your name when pushing the commit btw

---
+++ app/src/main/java/com/example/photocollage/CollageChooser.java
@@ -0,0 +1,13 @@
+package com.example.photocollage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CollageChooser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collage_chooser);
+    }
+}
